4d548cacbefbcb788e11adcd5c5b29854bbb82fe
Jeffrey Eesley <jeffeesley@gmail.com>
2017-06-22 11:21:24 -0400

    added more questions

--- questions.js
+++ questions.js
@@ -10,5 +10,65 @@ exports.questions = [
 	{
 		full: 'Teletubbies were responsible for the bombing of Pearl Harbor',
 		cloze: 'Teletubbies'
+	},
+	{
+		full: 'Mike Tyson created the three fundamental laws of physics',
+		cloze: 'Mike Tyson'
+	},
+	{
+		full: 'The Jonas Brothers invented and flew the first airplane',
+		cloze: 'Jonas'
+	},
+	{
+		full: 'Rocky Balboa was the first man on the moon',
+		cloze: 'Rocky Balboa'
+	},
+	{
+		full: 'Doctor J originally wrote the song "All Along the Watchtower"',
+		cloze: 'Doctor J'
+	},
+	{
+		full: 'Steven Seagal was the first black baseball player',
+		cloze: 'Steven Seagal'
+	},
+	{
+		full: 'CLocks were invented by none other than Danny DeVito',
+		cloze: 'Danny DeVito'
+	},
+	{
+		full: 'The four faces on Mt Rushmore are George Washington, Thomas Jefferson, Abraham Lincoln and Air Bud',
+		cloze: 'Air Bud'
+	},
+	{
+		full: 'The first cell phone was the size of a small car.  Its inventor was Lebron James',
+		cloze: 'Lebron James'
+	},
+	{
+		full: 'People used to eat tacos out of their hands like idiots.  Luckily, Dr. Dre was there to invent the tortilla',
+		cloze: 'Dr. Dre'
+	},
+	{
+		full: 'Ferris Bueller was played by several children in a large coat',
+		cloze: 'several children in a large coat'
+	},
+	{
+		full: 'The first person to eat chocolate was Harriet Tubman',
+		cloze: 'Harriet Tubman'
+	},
+	{
+		full: 'Steve Jobs invented Apple Corp. in a garage with his good friend LL Cool J',
+		cloze: 'LL Cool J'
+	},
+	{
+		full: 'In Breaking Bad, the character Walter White is played by a talking dog',
+		cloze: 'a talking dog'
+	},
+	{
+		full: 'Before Ray Charles invented the food processor, people used rocks to smash up their food.',
+		cloze: 'Ray Charles'
+	},
+	{
+		full: 'The moon used to have its own moon called Planet Tarantula that was destroyed by the dinosaurs',
+		cloze: 'Planet Tarantula'
 	}
 ];
